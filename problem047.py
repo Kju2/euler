719cbba0c09d80b0cbe5a0e8afe8e1e84130b3be
kju2 <kju2@edlinger.me>
2012-05-27 20:03:55 +0200

    solved problem 047

---
+++ problem047.py
@@ -0,0 +1,51 @@
+"""
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 * 7
+15 = 3 * 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2^2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct primes factors.
+What is the first of these numbers?
+"""
+
+from itertools import count
+from prime import prime_factors_of
+
+
+def main():
+    """
+    >>> main()
+    134043
+    """
+    needed_consecutive_valid_numbers = 4
+    distinct_prime_factors = 4
+
+    # next number after lower bound has to have at least distinct_prime_factors
+    # of odd primes as prime factors
+    lower_bound = (3 * 5 * 7 * 11) - 1
+
+    number = 0
+    consecutive_valid_numbers_count = 0
+
+    for number in count(lower_bound):
+        if consecutive_valid_numbers_count == needed_consecutive_valid_numbers:
+            break
+
+        if len(set(prime_factors_of(number))) == distinct_prime_factors:
+            consecutive_valid_numbers_count += 1
+        else:
+            consecutive_valid_numbers_count = 0
+
+    # print first number of the consecutive numbers
+    print(number - distinct_prime_factors)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
