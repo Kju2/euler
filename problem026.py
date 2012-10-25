94144a0bcba79cba498838ac4a2d4ca12daf052d
kju2 <kju2@edlinger.me>
2012-10-25 10:35:35 +0200

    improve solution for problem 026 - 027

--- problem026.py
+++ problem026.py
@@ -1,5 +1,6 @@
 """
-A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+A unit fraction contains 1 in the numerator. The decimal representation of the
+unit fractions with denominators 2 to 10 are given:
 
 1/2	= 	0.5
 1/3	= 	0.(3)
@@ -10,14 +11,19 @@ A unit fraction contains 1 in the numerator. The decimal representation of the u
 1/8	= 	0.125
 1/9	= 	0.(1)
 1/10	= 	0.1
-Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be
+seen that 1/7 has a 6-digit recurring cycle.
 
-Find the value of d  1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+Find the value of d  1000 for which 1/d contains the longest recurring cycle
+in its decimal fraction part.
 """
 
 """
 Wikipedia: Form of cyclic numbers
 http://en.wikipedia.org/wiki/Cyclic_number
 
-7, 17, 19, 23, 29, 47, 59, 61, 97, 109, 113, 131, 149, 167, 179, 181, 193, 223, 229, 233, 257, 263, 269, 313, 337, 367, 379, 383, 389, 419, 433, 461, 487, 491, 499, 503, 509, 541, 571, 577, 593, 619, 647, 659, 701, 709, 727, 743, 811, 821, 823, 857, 863, 887, 937, 941, 953, 971, 977, 983 ...
+7, 17, 19, 23, 29, 47, 59, 61, 97, 109, 113, 131, 149, 167, 179, 181, 193, 223,
+229, 233, 257, 263, 269, 313, 337, 367, 379, 383, 389, 419, 433, 461, 487, 491,
+499, 503, 509, 541, 571, 577, 593, 619, 647, 659, 701, 709, 727, 743, 811, 821,
+823, 857, 863, 887, 937, 941, 953, 971, 977, 983 ...
 """
--- problem027.py
+++ problem027.py
@@ -1,66 +1,65 @@
-from itertools import count, dropwhile, product
 """
 Euler published the remarkable quadratic formula:
 
 n**2 + n + 41
 
-It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41**2 + 41 + 41 is clearly divisible by 41.
+It turns out that the formula will produce 40 primes for the consecutive values
+n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible
+by 41, and certainly when n = 41, 41**2 + 41 + 41 is clearly divisible by 41.
 
-Using computers, the incredible formula  n**2  79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, 79 and 1601, is 126479.
+Using computers, the incredible formula  n**2  79n + 1601 was discovered, which
+produces 80 primes for the consecutive values n = 0 to 79. The product of the
+coefficients, 79 and 1601, is 126479.
 
 Considering quadratics of the form:
 
-n**2 + an + b, where |a|  1000 and |b|  1000
+n**2 + an + b, where |a| < 1000 and |b| < 1000
 
 where |n| is the modulus/absolute value of n
 e.g. |11| = 11 and |4| = 4
-Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.
+Find the product of the coefficients, a and b, for the quadratic expression
+that produces the maximum number of primes for consecutive values of n,
+starting with n = 0.
 """
 
-# primes_up_to from problem 10
-def primes_up_to(limit):
-    """the sieve of Eratosthenes"""
-    sieve_bound = (limit-1) // 2
-    sieve = range(0,sieve_bound)
-    cross_limit = int((limit**0.5 -1) / 2)
+from itertools import count, product
 
-    for i in range(cross_limit):
-        if sieve[i]: # 2*i + 1 is prime, mark multiples
-            for j in range(2*i*(i+1), sieve_bound, 2*i+1):
-                sieve[j] = 0
-            pass
-        pass
-    pass
-    return [2] + [2*p+1 for p in sieve if p]
+from prime import Primes
 
-def is_prime(p):
-    if p in is_prime.primes:
-        return True
-        pass
-    if p < is_prime.max_prime:
-        return False
-        pass
-    if p < 0:
-        return is_prime(abs(p))
-        pass
 
-    is_prime.primes = frozenset(primes_up_to(p*2))
-    is_prime.max_prime = max(is_prime.primes)
-    return is_prime(p)
-    pass
+def consecutive_primes_for(coefficient_a, coefficient_b, primes):
+    """
+    Comuptes the number of consecutive primes for the coefficients a and b.
+    """
+    count_consecutive_primes = 0
+    for n in count(0):
+        if (n ** 2 + coefficient_a * n + coefficient_b) not in primes:
+            break
+        count_consecutive_primes += 1
+    return count_consecutive_primes
 
-is_prime.primes = frozenset(primes_up_to(10**5))
-is_prime.max_prime = max(is_prime.primes)
 
-def consecutive_primes_for(a, b):
-    return next(dropwhile(lambda n: is_prime(n**2 + a*n + b), count(0)))-1
+def main():
+    """
+    >>> main()
+    -59231
+    """
+    primes = Primes(10 ** 6)
+    # 'a' needs to be odd, otherwise for every odd 'n', the result is even
+    a = xrange(-999, 1000, 2)
+    # 'b' needs to be prime, otherwise for n=0 the formula won't give a prime
+    b = (b for b in range(-999, 1000, 2) if b in primes)
 
-# 'a' needs to be odd, otherwise for every odd 'n', the result is even
-a = range(-999, 1000, 2)
-# 'b' needs to be prime, otherwise for n=0 the formula will never give a prime
-b = filter(is_prime, range(-999, 1000, 2))
+    max_consecutive_primes = 0
+    for (a, b) in product(a, b):
+        consecutive_primes = consecutive_primes_for(a, b, primes)
+        if consecutive_primes > max_consecutive_primes:
+            product_of_coefficients = a * b
+            max_consecutive_primes = consecutive_primes
 
-print max( \
-        map(lambda (a, b): (consecutive_primes_for(a, b), a * b), product(a, b)), \
-        key=(lambda (consecutive_primes, product_a_b): consecutive_primes) \
-        )[1]
+    print (product_of_coefficients)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
