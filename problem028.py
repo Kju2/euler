c0ed0e04114bb33d8a33466acc019009c2dc4bb7
kju2 <kju2@edlinger.me>
2012-10-29 00:04:21 +0100

    improved solution for problem 028

--- problem028.py
+++ problem028.py
@@ -1,5 +1,6 @@
 """
-Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+Starting with the number 1 and moving to the right in a clockwise direction
+a 5 by 5 spiral is formed as follows:
 
 21 22 23 24 25
 20  7  8  9 10
@@ -9,10 +10,22 @@ Starting with the number 1 and moving to the right in a clockwise direction a 5 
 
 It can be verified that the sum of the numbers on the diagonals is 101.
 
-What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed
+in the same way?
 """
 
-limit = 1001
 
-# n**2 is right corner
-print sum(map(lambda x: 4*(x**2) - 6*x + 6, range(3, limit+1, 2)))+ 1
+def main():
+    """
+    >>> main()
+    669171001
+    """
+    limit = 1001
+
+    # n ** 2 is right corner
+    print(sum([4 * (x ** 2) - 6 * x + 6 for x in range(3, limit + 1, 2)]) + 1)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
