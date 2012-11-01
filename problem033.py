841d035a20a1b62be061c6f40cf1bd773e89f83f
kju2 <kju2@edlinger.me>
2012-11-01 21:08:37 +0100

    improved solution for problem 033

--- problem033.py
+++ problem033.py
@@ -1,5 +1,3 @@
-from fractions import Fraction
-from operator import mul
 """
 The fraction 49/98 is a curious fraction, as an inexperienced mathematician in
 attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is
@@ -14,23 +12,33 @@ If the product of these four fractions is given in its lowest common terms,
 find the value of the denominator.
 """
 
-# not a curious fraction 30/50 or 77/77
-curious_fractions = []
+from fractions import Fraction
+from operator import mul
 
-for numerator_tens in range(1, 10, 1):
-    for denominator_unit in range(1, 10):
-        for cancler in range(1, 10):
-            numerator = numerator_tens * 10 + cancler
-            denominator = cancler * 10 + denominator_unit
 
-            f1 = Fraction(numerator, denominator)
-            f2 = Fraction(numerator_tens, denominator_unit)
+def main():
+    """
+    >>> main()
+    100
+    """
+    # not a curious fraction 30/50 or 77/77
+    curious_fractions = []
 
-            if f1 == f2 and numerator != denominator:
-                curious_fractions.append(f1)
-                pass
-            pass
-        pass
-    pass
+    for numerator_tens in range(1, 10):
+        for denominator_unit in range(1, 10):
+            for cancler in range(1, 10):
+                numerator = numerator_tens * 10 + cancler
+                denominator = cancler * 10 + denominator_unit
 
-print reduce(mul, curious_fractions).denominator
+                fraction1 = Fraction(numerator, denominator)
+                fraction2 = Fraction(numerator_tens, denominator_unit)
+
+                if fraction1 == fraction2 and numerator != denominator:
+                    curious_fractions.append(fraction1)
+
+    print(reduce(mul, curious_fractions).denominator)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
