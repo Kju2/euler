49ef0d5bee5bd1576185a3daf64f287718149384
kju2 <kju2@edlinger.me>
2012-11-02 11:34:09 +0100

    improved solution for problem 031 - 032

--- problem031.py
+++ problem031.py
@@ -1,5 +1,6 @@
 """
-In England the currency is made up of pound and pence, p, and there are eight coins in general circulation:
+In England the currency is made up of pound and pence, p, and there are eight
+coins in general circulation:
 
 1p, 2p, 5p, 10p, 20p, 50p, 1 Pound (100p) and 2 Pound (200p).
 It is possible to make 2 Pound in the following way:
@@ -8,17 +9,27 @@ It is possible to make 2 Pound in the following way:
 
 How many different ways can 2 Pound be made using any number of coins?
 """
-limit = 200
 
-coins =[1, 2, 5, 10, 20, 50, 100, 200]
 
-array = [0] * 201
-array[0] = 1
+def main():
+    """
+    >>> main()
+    73682
+    """
+    limit = 200
 
-for c in coins:
-    for n in xrange(c, limit+1):
-        array[n] += array[n-c]
-        pass
-    pass
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-print array[200]
+    array = [0] * 201
+    array[0] = 1
+
+    for c in coins:
+        for n in xrange(c, limit + 1):
+            array[n] += array[n - c]
+
+    print(array[200])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- problem032.py
+++ problem032.py
@@ -1,37 +1,58 @@
+"""
+We shall say that an n-digit number is pandigital if it makes use of all the
+digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1
+through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 * 186 = 7254, containing
+multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity
+can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only
+include it once in your sum.
+"""
+
 from itertools import count
 from math import log10
-"""
-We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
 
-The product 7254 is unusual, as the identity, 39 * 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
 
-Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+def digits_in(number):
+    """
+    >>> digits_in(1)
+    1
+    >>> digits_in(10)
+    2
+    >>> digits_in(111)
+    3
+    """
+    return int(log10(number)) + 1
 
-HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
-"""
 
-base = frozenset(["1","2","3","4","5","6","7","8","9"])
+def main():
+    """
+    >>> main()
+    45228
+    """
+    base = frozenset(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
-def digitsIn(n):
-    return int(log10(n)) + 1
-    pass
+    products = set([])
+    for x in range(2, 80):
+        for y in count(x + 1):
+            z = x * y
 
-products = set([])
-for x in range(2, 80):
-    for y in count(x+1):
-        z = x * y
-        
-        # is the count of digits not above 9
-        if digitsIn(x) + digitsIn(y) + digitsIn(z) > 9:
-            # can't be 1 trough 9 pandigital with more than 9 digits
-            break
-            pass
+            # is the count of digits not above 9
+            if digits_in(x) + digits_in(y) + digits_in(z) > 9:
+                # can't be 1 trough 9 pandigital with more than 9 digits
+                break
 
-        # is x, y and z combind pandigital
-        if base.issubset(str(x) + str(y) + str(z)):
-            products.add(z)
-            pass
-        pass
-    pass
+            # is x, y and z combind pandigital
+            if base.issubset(str(x) + str(y) + str(z)):
+                products.add(z)
 
-print sum(products)
+    print(sum(products))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
